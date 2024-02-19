890395f302cd78c3bd02a938223abbda00671fcc
Iku <Kakkela@users.noreply.github.com>
2024-02-19 20:07:09 +0200

    Update vdo-iracing-switcher.py

--- vdo-iracing-switcher.py
+++ vdo-iracing-switcher.py
@@ -12,8 +12,6 @@ class State:
 def check_iracing():
     if state.ir_connected and not (ir.is_initialized and ir.is_connected):
         state.ir_connected = False
-        # don't forget to reset your State variables
-        state.last_car_setup_tick = -1
         # we are shutting down ir library (clearing all internal variables)
         ir.shutdown()
         print('irsdk disconnected')
